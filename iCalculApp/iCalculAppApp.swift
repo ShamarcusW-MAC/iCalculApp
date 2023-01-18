b3544b228f4b0aef93b5ea14cd3b18bb5541613a
Sha'Marcus Walker <shamarcusw84@gmail.com>
2023-01-17 21:47:16 -0500

    Initial Commit

---
+++ iCalculApp/iCalculAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalculAppApp.swift
+//  iCalculApp
+//
+//  Created by Consultant on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct iCalculAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
